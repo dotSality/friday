438fc48d56a0e17b3047b530512c246b8df7d354
Toyfon <90236783+Toyfon@users.noreply.github.com>
2022-02-07 11:27:05 +0300

    Added logic from log out

--- src/AllRoutes/Pages/LoginPage/FormikLoginPage.tsx
+++ src/AllRoutes/Pages/LoginPage/FormikLoginPage.tsx
@@ -1,16 +1,25 @@
+import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+
 import {useFormik} from "formik";
 import {useAppSelector} from "../../../bll/store";
-import {NavLink, useNavigate} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {loginTC} from "../../../bll/login-reducer";
-import s from './Login.module.scss'
+
 import Paper from "@mui/material/Paper/Paper";
 import Typography from "@mui/material/Typography/Typography";
 import TextField from "@mui/material/TextField/TextField";
-import Fab from "@mui/material/Fab/Fab";
 import Container from "@mui/material/Container/Container";
 import Button from "@mui/material/Button/Button";
-import {Checkbox, FormControlLabel, FormGroup} from "@mui/material";
+import {Checkbox, FormControlLabel} from "@mui/material";
+import {loginTC} from "../../../bll/login-reducer";
+
+import s from './Login.module.scss'
+import {PATH} from "../../../utils/paths";
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
 
 
 export const LoginPage = () => {
@@ -25,16 +34,28 @@ export const LoginPage = () => {
             password: '',
             rememberMe: false,
         },
-        validate: () => {
-
+        validate: values => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'email is required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = 'password is required'
+            } else if (values.password.length < 5) {
+                errors.password = 'Must be 5 characters or more'
+            }
+            return errors;
         },
         onSubmit: values => {
             dispatch(loginTC(values))
+            formik.resetForm()
         }
     })
 
     if (isLoggedIn) {
-        navigate('/')
+        navigate(PATH.MAIN)
     }
 
     return <div className={s.mainContainer}>
@@ -55,6 +76,8 @@ export const LoginPage = () => {
                     variant='standard'
                     {...formik.getFieldProps('email')}
                 />
+                {formik.touched.email && formik.errors.email &&
+                <div style={{color: 'red', textAlign: 'center'}}>{formik.errors.email}</div>}
                 <TextField
                     sx={{width: '100%'}}
                     id='outlined-basic'
@@ -63,10 +86,12 @@ export const LoginPage = () => {
                     variant='standard'
                     {...formik.getFieldProps('password')}
                 />
+                {formik.touched.email && formik.errors.password && <
+                    div style={{color: 'red', textAlign: 'center'}}>{formik.errors.password}</div>}
                 <Typography
                     variant={'subtitle2'}
-                    sx={{cursor:'pointer'}}
-                    onClick={() => navigate('/pass-recover')}>
+                    sx={{cursor: 'pointer'}}
+                    onClick={() => navigate(PATH.PASS_RECOVER)}>
                     forgot your password?
                 </Typography>
                 <FormControlLabel
@@ -74,14 +99,16 @@ export const LoginPage = () => {
                     control={<Checkbox
                         {...formik.getFieldProps('rememberMe')}/>}
                 />
-                <Button type={'submit'} variant={'contained'}>Login</Button>
+                <Button type={'submit'}
+                        sx={{width: '100%'}}
+                        variant={'contained'}>Login</Button>
 
                 <Container className={s.signUp}>
                     <Typography variant={'subtitle1'} sx={{opacity: '50%'}}>
                         Don't have an account?
                     </Typography>
 
-                    <Button onClick={() => navigate('/register')}>Sign Up</Button>
+                    <Button onClick={() => navigate(PATH.REGISTER)}>Sign Up</Button>
                 </Container>
 
             </form>
