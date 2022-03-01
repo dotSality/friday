88c7da00c109928ef2fc9c7a7e3b291d56f66c9b
dotSality <90727519+dotSality@users.noreply.github.com>
2022-03-01 15:21:11 +0300

    fix

--- src/features/Cards/LearnCards/LearnCards.tsx
+++ src/features/Cards/LearnCards/LearnCards.tsx
@@ -1,44 +1,45 @@
 import s from './LearnCards.module.scss';
 import {memo, useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from '../../../bll/store';
-import {fetchCards, removeCard, setSum} from '../../../bll/cards-reducer';
+// import {fetchCards, removeCard, setSum} from '../../../bll/cards-reducer';
 import {useParams} from 'react-router-dom';
 import {NotAuthRedirect} from '../../../hoc/NotAuthRedirect';
 
 const Component = memo(() => {
 
     const currentCards = useAppSelector(state => state.cards.cardsData.cards)
-    const sum = useAppSelector(state => state.cards.sum)
+    // const sum = useAppSelector(state => state.cards.sum)
     const dispatch = useAppDispatch()
     const {cardsPack_id} = useParams()
     console.log(cardsPack_id)
-    useEffect(() => {
-        if (currentCards.length === 0 && cardsPack_id) {
-            dispatch(fetchCards({
-                cardsPack_id,
-                page: 1,
-                pageCount: 10,
-            }))
-        }
-    }, [currentCards])
+    // useEffect(() => {
+    //     if (currentCards.length === 0 && cardsPack_id) {
+    //         dispatch(fetchCards({
+    //             cardsPack_id,
+    //             page: 1,
+    //             pageCount: 10,
+    //         }))
+    //     }
+    // }, [currentCards])
 
     const randomIndex = Math.floor(Math.random() * currentCards.length)
-    const card = currentCards.filter(c => c.grade < 7 - sum)[randomIndex]
-    console.log(sum)
-    const nextCard = () => {
-        if (sum < 7) {
-            dispatch(setSum(sum + card.grade))
-        } else {
-            dispatch(setSum(0))
-        }
-        dispatch(removeCard(card._id))
-    }
+    // const card = currentCards.filter(c => c.grade < 7 - sum)[randomIndex]
+    // console.log(sum)
+    // const nextCard = () => {
+    //     if (sum < 7) {
+    //         dispatch(setSum(sum + card.grade))
+    //     } else {
+    //         dispatch(setSum(0))
+    //     }
+    //     dispatch(removeCard(card._id))
+    // }
 
     return (
         <div className={s.mainContainer}>
             <div className={s.container}>
-                {sum && sum}
-                <button onClick={nextCard}>Next</button>
+                {/*{sum && sum}*/}
+                {/*<button onClick={nextCard}>Next</button>*/}
+                NOTHING
             </div>
         </div>
     )
